f92b243389978d54c41eeee8fb462689461f47cb
Oliver Douglas <ofdouglas@gmail.com>
2021-03-04 18:05:47 -0800

    Add pb codec, set up basic demo (NAK not working)

--- makefile
+++ makefile
@@ -10,8 +10,9 @@ PROTO_C = 	$(patsubst $(SRC_DIR)/%.proto, $(SRC_DIR)/%.pb.c, $(PROTO_SRC))
 PROTO_H = 	$(patsubst $(SRC_DIR)/%.proto, $(SRC_DIR)/%.pb.h, $(PROTO_SRC))
 CPP_OBJ = 	$(patsubst $(SRC_DIR)/%.cpp, $(BUILD_DIR)/%.o, $(CPP_SRC))
 PROTO_OBJ =	$(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(PROTO_C))
+NANOPB_OBJ = $(patsubst $(NANOPB_DIR)/%.c, $(BUILD_DIR)/%.o, $(NANOPB_CORE))
 
-CPPFLAGS += -Inanopb -I$(PROJ_ROOT_DIR)
+CPPFLAGS += -Inanopb -I$(PROJ_ROOT_DIR) -g
 
 .PHONY: all clean proto
 all: main
@@ -19,14 +20,17 @@ all: main
 DEPENDENCIES = $(PROTO_OBJ:.o=.d) $(CPP_OBJ:.o=.d)
 -include $(DEPENDENCIES)
 
-$(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp $(PROTO_C) $(PROTO_H)
+$(CPP_OBJ): $(BUILD_DIR)/%.o: $(SRC_DIR)/%.cpp $(PROTO_C) $(PROTO_H)
 	$(CXX) $(CPPFLAGS) -MMD -c $< -o $@
 
 $(BUILD_DIR)/%.pb.o: $(SRC_DIR)/%.pb.c
 	$(CXX) $(CPPFLAGS) -c $< -o $@
 
-main: $(CPP_OBJ) $(PROTO_OBJ)
-	$(CXX) $(CPPFLAGS) $(PROTO_OBJ) $(CPP_OBJ) -o $@
+$(NANOPB_OBJ): $(BUILD_DIR)/%.o: $(NANOPB_DIR)/%.c
+	$(CXX) $(CPPFLAGS) -c $< -o $@
+
+main: $(CPP_OBJ) $(PROTO_OBJ) $(NANOPB_OBJ)
+	$(CXX) $(CPPFLAGS) $(PROTO_OBJ) $(CPP_OBJ) $(NANOPB_OBJ) -o $@
 
 clean:
 	@find ./ -iregex '.*\.[od]$$' -exec rm {} +
--- src/main.cpp
+++ src/main.cpp
@@ -4,70 +4,146 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
-
+#include <cassert>
+#include <cstdlib>
+#include <ctime>
 #include "definitions.pb.h"
 #include "protobuf_codec.tpp"
 
 
-/*
-* Read binary file (server)
-* Generate multiple FirmwareImagePage messages
-* Send each page to device
-* Device decodes and returns a response (true iff CRC correct)
-*
-* Simulation: spawn two threads. Have the server read a
-* binary file, encode&send chunk by chunk, to device.
-*/
+static const char* socket_path = "temporary_unix_socket";
+constexpr size_t page_size = sizeof(((gridware_FirmwareImagePage*)0)->page.bytes);
+static const bool enable_bit_errors = false;
 
 void err_exit(const char* s) {
     perror(s);
     exit(-1);
 }
 
-static const char* socket_path = "temporary_unix_socket";
-
-void server_process(int sock_fd, std::ifstream& fw_image) {
-    std::cout << "Server process " << getpid() << " starting\n";
-
-    for (int i = 0; i < 10; i++) {
-        const char* msg = "hello\n";
-        int res = send(sock_fd, msg, strlen(msg) + 1, 0);
-        //sleep(1);
-    }
-
-    std::cout << "Server process exiting\n";
+uint16_t crc16(char* data, size_t len) {
+    return ~len;   // TODO: implement
 }
 
-void device_process(int sock_fd, pid_t ppid) {
-    std::cout << "Device process " << getpid() << " starting\n";
+bool server_send_page_attempt(int sock_fd, gridware_FirmwareImagePage& page) {
+    ProtoBufCodec<gridware_FirmwareImagePage> page_codec;
+    ProtoBufCodec<gridware_DeviceResponse> response_codec;
+    gridware_DeviceResponse response;
+    uint8_t msg_buf[page_codec.MaxEncodedSize()];
+    uint8_t rsp_buf[response_codec.MaxEncodedSize()];
 
-    char buf[1024] = {};
-    int n = 0;
-    size_t total = 0;
+    uint16_t crc = crc16(reinterpret_cast<char*>(&page.page.bytes), page.page.size);
+    memcpy(page.crc.bytes, &crc, sizeof(uint16_t));
+    page.crc.size = sizeof(uint16_t);
 
-    while (getppid() == ppid) { // PPID will change when the original parent dies
-         n = recv(sock_fd, buf, sizeof(buf), MSG_DONTWAIT);
+    int bytes_encoded = page_codec.Encode(&page, msg_buf, sizeof(msg_buf));
+    assert(bytes_encoded > 0);
 
-         if (n > 0) {
-            std::cout << "Device received " << n << " bytes\n";
-             for (size_t i = 0; i < n; i++)
-                putchar(buf[i]);
-             total += n;
-         }
+    send(sock_fd, msg_buf, bytes_encoded, 0);
+    std::cout << "Server: TX " << page.page.size << " bytes\n";
+
+    int n = recv(sock_fd, rsp_buf, sizeof(rsp_buf), 0);
+    if (n > 0) {
+        int res = response_codec.Decode(&response, rsp_buf, n);
+        if (!response.verified) {
+            std::cout << "Server: page rejected\n";
+        } else {
+            std::cout << "Server: page accepted\n";
+        }
+        return response.verified;
+    } else {
+        err_exit("Server: connection failure");
+        return false;
+    }
+}
+
+void server_process(int sock_fd, std::ifstream& file) {
+    gridware_FirmwareImagePage page;
+
+    while (file) {
+        file.read(reinterpret_cast<char*>(&page.page.bytes), page_size);
+        page.page.size = file.gcount();
+        if (page.page.size < page_size) {
+            page.last = true;
+            assert(file.eof());
+        }
+
+        bool success = false;
+        for (int i = 0; !success && i < 3; i++) {
+            success = server_send_page_attempt(sock_fd, page);
+        }
+
+        if (!success) {
+            std::cout << "Server: Maximum page retry exceeded. Transfer failed\n";
+            return;
+        }
+    }
+}
+
+void device_process(int sock_fd, std::ofstream& file) {
+    ProtoBufCodec<gridware_FirmwareImagePage> page_codec;
+    ProtoBufCodec<gridware_DeviceResponse> response_codec;
+    gridware_FirmwareImagePage page;
+    gridware_DeviceResponse response;
+    uint8_t msg_buf[page_codec.MaxEncodedSize()];
+    uint8_t rsp_buf[response_codec.MaxEncodedSize()];
+    srand(time(NULL));
+
+    while (true) {
+        int n = recv(sock_fd, msg_buf, sizeof(msg_buf), 0);
+        if (n <= 0) {
+            err_exit("Device: connection failure");
+        } else {
+            int res = page_codec.Decode(&page, msg_buf, n);
+
+            uint16_t expected_crc = crc16(reinterpret_cast<char*>(&page.page.bytes), page.page.size);
+            uint16_t msg_crc;
+            memcpy(&msg_crc, page.crc.bytes, sizeof(uint16_t));
+
+            if (enable_bit_errors && !(rand() % 5)) {
+                msg_crc ^= 0x1234;
+            }
+
+            if (msg_crc == expected_crc) {
+                std::cout << "Device: RX " << page.page.size << " bytes\n";
+                file.write(reinterpret_cast<char*>(&page.page.bytes), page.page.size);
+                response.verified = true;
+            } else {
+                std::cout << "Invalid CRC: expected " << expected_crc << ", got " << msg_crc << "\n";
+
+                // TODO: this doesn't work. For some reason, pb_encode writes zero bytes, but reports
+                // success, when encoding a gridware_DeviceResponse with 'verified=false'.
+                response.verified = false;
+            }
+
+            res = response_codec.Encode(&response, rsp_buf, sizeof(rsp_buf));
+            assert(res > 0); // TODO: Encoding always fails if response.verified is false.
+            if (res > 0) {
+                send(sock_fd, rsp_buf, res, 0);
+            }
+
+            if (page.last && response.verified) {
+                break;
+            }
+        }
     }
 
-    std::cout << "Device processing exiting, " << n << " bytes received\n";
+    file.close();
 }
 
 int main(int argc, const char* argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage " << argv[0] << " filename" << std::endl;;
+    if (argc != 3) {
+        std::cout << "Usage " << argv[0] << " infile outfile\n";
         exit(-1);
     }
 
     std::ifstream input_file(argv[1]);
     if (!input_file) {
-        err_exit("Unable to read input file");
+        err_exit("Unable to open input file");
+    }
+
+    std::ofstream output_file(argv[2]);
+    if (!output_file) {
+        err_exit("Unable to open output file");
     }
 
     enum { DEVICE_SOCKET, SERVER_SOCKET };
@@ -77,14 +153,17 @@ int main(int argc, const char* argv[]) {
         err_exit("Socketpair failure\n");
     }
 
-    pid_t ppid = getpid();
     pid_t res = fork();
     if (res < 0) {
         err_exit("Fork failure");
-    } else if (res == 0) {  
-        device_process(sock_fd[DEVICE_SOCKET], ppid);        // Child: device process
+    } else if (res == 0) {
+        std::cout << "Device: PID " << getpid() << " starting\n";
+        device_process(sock_fd[DEVICE_SOCKET], output_file);
+        std::cout << "Device: PID " << getpid() << " exiting \n";
     } else {                
-        server_process(sock_fd[SERVER_SOCKET], input_file);  // Parent: server process
+        std::cout << "Server: PID " << getpid() << " starting\n";
+        server_process(sock_fd[SERVER_SOCKET], input_file);
+        std::cout << "Server: PID " << getpid() << " exiting \n";
     }
 
     return 0;
